98fa552ac2f5156df5d36e04e354fffabfb6101a
Dev <joisardev@gmail.com>
2022-01-16 14:29:56 +0530

    Checks if input character is vowel or not

---
+++ If_Demo/Vowel_Consonant.py
@@ -0,0 +1,5 @@
+char=input("Enter Alphabet=>")
+if char=="A" or char=="a" or char=="E" or char=="e" or char=="I" or char=="i" or char=="O" or char=="o" or char=="U" or char=="u":
+    print("Alphabet",char,"is a vowel")
+else:
+    print("Alphabet",char,"is a consonant")
