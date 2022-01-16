c52cfe684390f0bc1833f0462e3901625b0a5d05
Dev <joisardev@gmail.com>
2022-01-16 16:06:43 +0530

    All syntax under math module

---
+++ Modules/Module-MathDemo.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.sin(0))
+print(math.cos(0))
+print(math.sqrt(25))
+print(math.trunc(223.4589))
+print(math.pow(2,5))
+print(math.floor(25.90))
+print(math.floor(-25.90))
+print(math.ceil(25.90))
+print(math.ceil(-25.90))
