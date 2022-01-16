e976b451b92f699d23f305a94c6c8429f268d98b
Dev <joisardev@gmail.com>
2022-01-16 15:34:26 +0530

    Function with Arguments

---
+++ Function/Function-WithArguments.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    print("Addition=>",a+b)
+def sub(a,b):
+     print("Subtraciton=>",a-b)
+def mul(a,b):
+     print("Multiplication=>",a*b)
+def div(a,b):
+    print("Division=>",a/b)
+x=int(input("Enter no1 =>"))
+y=int(input("Enter no2 =>"))
+add(x,y)
+sub(x,y)
+div(x,y)
+mul(x,y)
