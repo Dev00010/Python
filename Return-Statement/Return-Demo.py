849416185ac5e4915729ea7b5b92b5e69e81f1e1
Dev <joisardev@gmail.com>
2022-01-16 16:07:30 +0530

    Demo of a Return Function

---
+++ Return-Statement/Return-Demo.py
@@ -0,0 +1,7 @@
+def calc(x,y):
+    return x + y, x - y, x / y, x * y    #in python you can return many value in single
+a,s,m,d=calc(22,2)
+print("Add = ",a)
+print("Sub = ",s)
+print("Mul = ",m)
+print("Div = ",d)
